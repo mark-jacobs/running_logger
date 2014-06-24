620489d1a2a7cdab1372d1fd55a66a58f472a474
mark-jacobs <your_email@example.com>
2014-06-24 17:49:36 +0100

    add very basic plan functionality

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -1,2 +1,26 @@
 class TrainingPlansController < ApplicationController
+  def new
+    @user = current_user
+    @training_plan = @user.training_plans.build
+  end
+
+  def create
+    @user = current_user
+    if @training_plan = @user.training_plans.create(training_plan_params)
+      redirect_to root_url
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @user = current_user
+    @training_plans = @user.training_plans
+  end
+
+  private
+
+    def training_plan_params
+      params.require(:training_plan).permit(:plan_date, :plan_miles, :plan_workout, :plan_q)
+    end
 end
