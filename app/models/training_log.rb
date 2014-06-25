63df98af73fb2e365e0ea4a4b2c75ac689ed256d
mark-jacobs <your_email@example.com>
2014-06-25 14:02:58 +0100

    change validations on log and plan to work with user_id

--- app/models/training_log.rb
+++ app/models/training_log.rb
@@ -1,6 +1,7 @@
 class TrainingLog < ActiveRecord::Base
   validates :user_id, presence: true
-  validates :log_date, presence: true, uniqueness: true
+  validates :log_date, presence: true, uniqueness: { scope: :user_id, 
+        message: "Only one session can be planned for a day." }
 
   belongs_to :user
 end
--- app/models/training_plan.rb
+++ app/models/training_plan.rb
@@ -1,6 +1,7 @@
 class TrainingPlan < ActiveRecord::Base
   validates :user_id, presence: true
-  validates :plan_date, presence: true
+  validates :plan_date, presence: true, uniqueness: { scope: :user_id, 
+        message: "Only one session can be planned for a day." }
 
   belongs_to :user
 end
