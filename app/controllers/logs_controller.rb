9fbb588ad3dd6f7b5348e8936d48a8583e2b3937
mark-jacobs <your_email@example.com>
2014-07-03 17:33:05 +0100

    change rest of logs_controller to use logsManager and remove LogsHelper code

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,12 +1,11 @@
 class LogsController < ApplicationController
-  include LogsHelper
   before_action :signed_in_user
   before_action :correct_user
-  before_action :set_user, only: [:new, :create]
+  before_action :set_user, only: [:new, :create, :update]
   before_action :set_period, only: [:new, :log, :edit]
 
   def new
-    @logs = @user.logs.build(log_date: (startperiod(params[:period].to_i)) + (params[:day].to_i - 1).days)
+    @logs = @user.logs.build(log_date: (LogsManager.startperiod(params[:period].to_i)) + (params[:day].to_i - 1).days)
   end
 
   def create
@@ -27,13 +26,12 @@ class LogsController < ApplicationController
 
   def edit
     @user = User.find(params[:user_id])
-    @date = startperiod(params[:period].to_i).beginning_of_day + (params[:day].to_i - 1).days
+    @date = LogsManager.startperiod(params[:period].to_i).beginning_of_day + (params[:day].to_i - 1).days
     @log = @user.logs.where("log_date >= ? AND log_date <= ?", @date.beginning_of_day , @date.end_of_day).first
     
   end
 
   def update
-    @user = current_user
     @log = @user.logs.find(params[:id])
     if @log.update(logs_params)
       flash[:success] = "Log updated"
