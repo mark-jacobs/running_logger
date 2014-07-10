cadaff4d790b56b0fa225278d9b0eae228d00c61
mark-jacobs <your_email@example.com>
2014-07-10 15:08:16 +0100

    add validations to plan_miles and log_miles to ensure numericality within the range 1..100 or nil

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,9 @@
 class Log < ActiveRecord::Base
   validates :log_date, presence: true, uniqueness: { scope: :user_id, 
         message: "Only one session can be planned for a day." }
-
+  validates :plan_miles, :inclusion => { :in => 0..100, message: ", range of 1 to 100 miles only"}, 
+            numericality: true, allow_nil: true
+  validates :log_miles, :inclusion => { :in => 0..100, message: ", range of 1 to 100 miles only"}, 
+            numericality: true, allow_nil: true
   belongs_to :user
 end
