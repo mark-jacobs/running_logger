54d1ff87e65dc5ff941e258a1bd1eb3574d42e24
mark-jacobs <your_email@example.com>
2014-07-15 12:21:13 +0100

    get weeks in phase to display on index

--- app/managers/phases_manager.rb
+++ app/managers/phases_manager.rb
@@ -22,15 +22,17 @@ class PhasesManager
 
     def get_weeks(a_phase)
       if a_phase.start_date.wday != 1
-        start = a_phase.start_date - a_phase.start_date.wday.days + 1.week
+        start = a_phase.start_date - a_phase.start_date.wday.days + 1.day + 1.week
       else
         start = a_phase.start_date
       end
       if a_phase.target_date.wday != 7
-        target = a_phase.target_date - a_phase.target_date.wday.days + 1.week
+        target = a_phase.target_date - a_phase.target_date.wday.days - 1.day + 1.week
       else
         target = a_phase.target_date
       end
-      return weeks = (target.beginning_of_day.to_i - start.beginning_of_day.to_i) / 1.week.to_i
+      weeks = (target.beginning_of_day.to_i - start.beginning_of_day.to_i) / 1.weeks.to_i
+      weeks = 0 unless weeks > 0
+      return weeks
     end
 end
