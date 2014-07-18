6aa72a5a064b69316b5a2eabb8783e0f3aa044c3
mark-jacobs <your_email@example.com>
2014-07-18 18:42:20 +0100

    localize text on log_import

--- spec/models/phase_spec.rb
+++ spec/models/phase_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe Phase, :type => :model do
   before (:each) do 
     phase = FactoryGirl.create :phase
-  
+    
     it "should be valid" do
       expect(phase).to be_valid
-    end 
+    end
+
+    it "should have a starting date of asdfdsfasdf" do
+      expect(phase).to_not be_valid
+    end
   end
 end
