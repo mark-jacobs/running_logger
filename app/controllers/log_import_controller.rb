9ab1435293067737abf3cf3e8e0e3e0b56f4db6c
mark-jacobs <your_email@example.com>
2014-07-18 12:37:17 +0100

    csv import slurps csv instead of reading path.

--- app/controllers/log_import_controller.rb
+++ app/controllers/log_import_controller.rb
@@ -1,21 +1,47 @@
 class LogImportController < ApplicationController
   require 'csv'
+
   def new
 
   end
   ###########################Don't forget to create transactions for import.
   def create
-    csv_path = params[:file].path unless params[:file].nil?
-    if csv_path.nil?
+    ##########################################################
+    ## This works but uses the path to open the csv....
+    # csv_path = params[:file].path unless params[:file].nil?
+    # if csv_path.nil?
+    #   render 'new'
+    # else
+    #   user = current_user
+    #   log_params = {}
+    #   CSV.foreach(csv_path, headers: true) do |row|
+    #     log_params[:log_date] = row[0]
+    #     log_params[:plan_miles] = row[1]
+    #     log_params[:plan_workout] = row[2]
+    #     log_params[:log_miles] = row[3]
+    #     log_params[:log_workout] = row[4]
+    #     log_params[:log_calories] = row[5]
+    #     if row[6].nil?
+    #       log_params[:log_time] = "00:00:00"
+    #     else
+    #       log_params[:log_time] = row[6]
+    #     end
+    #     log_params[:notes] = row[7]
+    #     new_log = user.logs.build(log_params)
+    #     new_log.save
+    #   end
+    #   redirect_to root_url, notice: "Yayy!"  
+    # end  
+    ##########################################################
+    if params[:file].nil?
       render 'new'
     else
-
-      #csv = CSV.parse(csv_text, headers: true, col_sep: ',')
+      csv_string = params[:file].read
+      csv = CSV.parse(csv_string, encoding: "UTF-8", headers: true)
       user = current_user
-      log_params = {}
-      
-      #CSV.foreach('/users/markjacobs/documents/csvimportsheet.csv', headers: true) do |row|
-      CSV.foreach(csv_path, headers: true) do |row|
+      count = 0
+      csv.each do |row|
+        log_params = {}
         log_params[:log_date] = row[0]
         log_params[:plan_miles] = row[1]
         log_params[:plan_workout] = row[2]
@@ -30,9 +56,10 @@ class LogImportController < ApplicationController
         log_params[:notes] = row[7]
         new_log = user.logs.build(log_params)
         new_log.save
+        puts "Added row #{count}"
+        count += 1
       end
-      redirect_to root_url, notice: "Yayy!"  
-    end  
-    
+      redirect_to "/users/#{current_user.id}/log/0", notice: "Logs imported"
+    end
   end
 end
