9a2ccdcab7607deb95f2b0c71b319cfb5428ce29
mark-jacobs <your_email@example.com>
2014-07-18 14:25:15 +0100

    Wrap log import into a transaction so that if it fails it is rolled back. Also display an error message indicating row failed on.

--- app/controllers/log_import_controller.rb
+++ app/controllers/log_import_controller.rb
@@ -4,62 +4,41 @@ class LogImportController < ApplicationController
   def new
 
   end
-  ###########################Don't forget to create transactions for import.
+
   def create
-    ##########################################################
-    ## This works but uses the path to open the csv....
-    # csv_path = params[:file].path unless params[:file].nil?
-    # if csv_path.nil?
-    #   render 'new'
-    # else
-    #   user = current_user
-    #   log_params = {}
-    #   CSV.foreach(csv_path, headers: true) do |row|
-    #     log_params[:log_date] = row[0]
-    #     log_params[:plan_miles] = row[1]
-    #     log_params[:plan_workout] = row[2]
-    #     log_params[:log_miles] = row[3]
-    #     log_params[:log_workout] = row[4]
-    #     log_params[:log_calories] = row[5]
-    #     if row[6].nil?
-    #       log_params[:log_time] = "00:00:00"
-    #     else
-    #       log_params[:log_time] = row[6]
-    #     end
-    #     log_params[:notes] = row[7]
-    #     new_log = user.logs.build(log_params)
-    #     new_log.save
-    #   end
-    #   redirect_to root_url, notice: "Yayy!"  
-    # end  
-    ##########################################################
     if params[:file].nil?
       render 'new'
     else
-      csv_string = params[:file].read
-      csv = CSV.parse(csv_string, encoding: "UTF-8", headers: true)
-      user = current_user
+      msg = "Imported!"
       count = 0
-      csv.each do |row|
-        log_params = {}
-        log_params[:log_date] = row[0]
-        log_params[:plan_miles] = row[1]
-        log_params[:plan_workout] = row[2]
-        log_params[:log_miles] = row[3]
-        log_params[:log_workout] = row[4]
-        log_params[:log_calories] = row[5]
-        if row[6].nil?
-          log_params[:log_time] = "00:00:00"
-        else
-          log_params[:log_time] = row[6]
+      begin
+        ActiveRecord::Base.transaction do
+          csv_string = params[:file].read
+          csv = CSV.parse(csv_string, encoding: "UTF-8", headers: true)
+          user = current_user
+          csv.each do |row|
+            count += 1
+            log_params = {}
+            log_params[:log_date] = row[0]
+            log_params[:plan_miles] = row[1]
+            log_params[:plan_workout] = row[2]
+            log_params[:log_miles] = row[3]
+            log_params[:log_workout] = row[4]
+            log_params[:log_calories] = row[5]
+            if row[6].nil?
+              log_params[:log_time] = "00:00:00"
+            else
+              log_params[:log_time] = row[6]
+            end
+            log_params[:notes] = row[7]
+            new_log = user.logs.build(log_params)
+            new_log.save!
+          end
         end
-        log_params[:notes] = row[7]
-        new_log = user.logs.build(log_params)
-        new_log.save
-        puts "Added row #{count}"
-        count += 1
+      rescue Exception => e
+        msg = e.message + " at row #{count}"
       end
-      redirect_to "/users/#{current_user.id}/log/0", notice: "Logs imported"
+      redirect_to "/users/#{current_user.id}/log/0", notice: "Error: #{msg}"
     end
   end
 end
